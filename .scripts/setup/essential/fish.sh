ef17f60083a742a065dd39d61bd70a30e38875b5
Miles Romeno <151898299+milesromeno@users.noreply.github.com>
2024-01-08 23:30:20 +0700

    change `brew` bin location to `/opt/homebrew` from `~/homebrew/`

--- .scripts/setup/essential/fish.sh
+++ .scripts/setup/essential/fish.sh
@@ -2,4 +2,4 @@ cd ~
 brew install fish
 chsh -s /opt/homebrew/bin/fish
 fish
-fish_add_path ~/homebrew/bin/
+fish_add_path /opt/homebrew/bin/
--- .scripts/setup/essential/homebrew.sh
+++ .scripts/setup/essential/homebrew.sh
@@ -1,3 +1,3 @@
 cd ~
-mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-echo 'export PATH="$HOME/homebrew/bin:$PATH"' >> .zprofile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zprofile
